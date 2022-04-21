87d6f2bb720cb8022a11d91f05dc16ddbf5605a4
Abhishek Nagekar <abhishek@nagekar.com>
2022-04-21 17:32:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const shuffleQuote = () => {
       const html_content = doc.content.rendered;
       const div = document.createElement('div')
       div.innerHTML = html_content;
-      const quoteList = div.querySelector('ol');
-      const quoteCount = quoteList.children.length;
+      const quoteList = div.querySelector('ol').children;
+      const quoteCount = quoteList.length;
       const randomNumber = Math.floor(Math.random() * (quoteCount + 1));
       const randomQuote = quoteList[randomNumber].innerText;
       quoteElem.innerHTML = randomQuote;
