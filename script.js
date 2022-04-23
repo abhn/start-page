26c533351f0d8fa8144d5954c8a8b91aa9205496
Abhishek Nagekar <abhishek@nagekar.com>
2022-04-24 00:19:42 +0200

    Fixed nested quotes error

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
+let quoteList = [];
+
+const setQuote = quote => {
+  const quoteElem = document.querySelector('#quote');
+  quoteElem.innerHTML = quote;
+}
+
+const getRandomQuote = quoteList => {
+  const index = Math.floor(Math.random() * quoteList.length);
+  return quoteList[index];
+}
+
+const selectQuote = () => {
+  const randomQuote = getRandomQuote(quoteList);
+
+  // test if it is a nested quote
+  const div = document.createElement('div');
+  div.innerHTML = randomQuote;
+  
+  if(div.childElementCount > 0 && div.querySelector('ol')) {
+    const innerQuoteList = div.querySelector('ol').children;
+    const randomInnerQuote = getRandomQuote(innerQuoteList);
+    setQuote(randomInnerQuote.innerText);
+  }
+  else {
+    setQuote(randomQuote.innerText);
+  }
+}
+
 const shuffleQuote = () => {
-  fetch('https://www.nagekar.com//wp-json/wp/v2/pages/1240')
-    .then(res => res.json())
-    .then(doc => {
-      let quoteElem = document.querySelector('#quote');
-      const html_content = doc.content.rendered;
-      const div = document.createElement('div')
-      div.innerHTML = html_content;
-      const quoteList = div.querySelector('ol').children;
-      const quoteCount = quoteList.length;
-      const randomNumber = Math.floor(Math.random() * (quoteCount + 1));
-      const randomQuote = quoteList[randomNumber].innerText;
-      quoteElem.innerHTML = randomQuote;
-    })
+  if(quoteList.length > 0) {
+    selectQuote();
+  }
+  else {
+    fetch('https://www.nagekar.com//wp-json/wp/v2/pages/1240')
+      .then(res => res.json())
+      .then(doc => {
+        const htmlContent = doc.content.rendered;
+        const div = document.createElement('div')
+        div.innerHTML = htmlContent;
+        quoteList = div.querySelector('ol').children;
+        selectQuote();
+      })
+  }
 }
 
 const tick = () => {
